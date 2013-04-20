56960db5b3cbd41179e932860de8b9e32886d201
Matt Reiferson <mreiferson@gmail.com>
2013-04-20 09:57:41 -0400

    bump to v0.2.19

--- nsq.rb
+++ nsq.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Nsq < Formula
   homepage 'https://github.com/bitly/nsq'
-  url 'https://s3.amazonaws.com/bitly-downloads/nsq/nsq-0.2.18.darwin-amd64.tar.gz'
-  version '0.2.18'
-  sha1 '63333c5d54a66bda03b2327b15d8a98eb9989b9d'
+  url 'https://s3.amazonaws.com/bitly-downloads/nsq/nsq-0.2.19.darwin-amd64.tar.gz'
+  version '0.2.19'
+  sha1 'aaa38e19b1ce5798aed98bead7311f8527f57a8b'
 
   def install
     bin.install Dir['bin/*']
